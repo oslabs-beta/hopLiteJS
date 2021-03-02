8a1ce977520d26c3336add02eee91f976fc65d30
reed182@mail.chapman.edu <reed182@mail.chapman.edu>
2021-03-02 14:51:32 -0800

    Boilerplate for hopLiteJS added.

---
+++ README.md
@@ -0,0 +1,1 @@
+#hopLiteJS Project
---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const path = require('path');
+
+const outputDirectory = 'dist';
+
+module.exports = {
+  entry: ['babel-polyfill', './src/hopLiteJS/index.ts'],
+  output: {
+    path: path.join(__dirname, outputDirectory),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.(png|woff|woff2|eot|ttf|svg)$/,
+        loader: 'url-loader?limit=100000'
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+};
