f611b91ded86450350e213b2eaeaf423f9a15efb
karin illesova <karin.illesova@gmail.com>
2021-03-11 05:50:10 +0100

    bcrypt functionality added to the library and tested

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,18 @@
 import DefaultHopliteBlueprint  from './modes/DefaultHoplite';
 import { AuthorizationControllerBlueprint } from './authorization/AuthorizationController';
 import { AuthenticationControllerBlueprint } from './authentication/AuthenticationController';
-import {HopliteUserSchemaBlueprint} from './modes/HopliteUserSchema'
+import { HopliteUserSchemaBlueprint} from './modes/HopliteUserSchema'
+import { HashingMethodsBlueprint } from './hashing/HashMethods';
+
 const AuthorizationController = new AuthorizationControllerBlueprint();
 const AuthenticationController = new AuthenticationControllerBlueprint();
 const DefaultHoplite = new DefaultHopliteBlueprint(AuthorizationController, AuthenticationController);
 const HopliteUserSchema = new HopliteUserSchemaBlueprint();
+const HashMethods = new HashingMethodsBlueprint();
+
+
 export {
   DefaultHoplite,
-  HopliteUserSchema
+  HopliteUserSchema,
+  HashMethods
 };
